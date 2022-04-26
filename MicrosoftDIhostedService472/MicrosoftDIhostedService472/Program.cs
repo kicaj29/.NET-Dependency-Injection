0403255f3fb5d9c6150551d45241f451dd8c804d
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-04-26 15:50:05 +0200

    MicrosoftDIhostedService472

---
+++ MicrosoftDIhostedService472/MicrosoftDIhostedService472/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftDIhostedService472
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (IHost host = CreateHostBuilder(args).Build())
+            {
+                using (_ = host.Services.CreateScope())
+                {
+                    await host.RunAsync();
+                }
+
+            }
+            Console.WriteLine("Initialization done");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IRecognitionManager, RecognitionManager>();
+                    services.AddHostedService<RecognitionMicroService>();
+                });
+    }
+}
---
+++ MicrosoftDIhostedService472/MicrosoftDIhostedService472/RecognitionMicroService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicrosoftDIhostedService472
+{
+    public class RecognitionMicroService : IHostedService
+    {
+        private IRecognitionManager _manager;
+        public RecognitionMicroService(IRecognitionManager manager)
+        {
+            this._manager = manager;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
